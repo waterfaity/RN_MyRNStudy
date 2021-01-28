a1df4f4d045ec92c3448ee82ab05fdd4490e4d12
xuchangwei <>
2021-01-28 16:50:10 +0800

    1.保存数据

--- myRNStudy/src/http/HttpConfig.js
+++ myRNStudy/src/http/HttpConfig.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://www.waterfairy.xyz";
+//export const BASE_URL = "http://www.waterfairy.xyz";
+export const BASE_URL = "http://172.20.24.31:8080";
--- myRNStudy/src/pages/LoginPage.js
+++ myRNStudy/src/pages/LoginPage.js
@@ -1,146 +1,194 @@
 import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Image, StatusBar, StyleSheet, TextInput, View } from "react-native";
 import Button from "../components/Button";
 import LoadingDialog from "../dialog/LoadingDialog";
 import { BASE_URL } from "../http/HttpConfig";
 
 export default class LoginPage extends React.Component {
-  dialogRef = React.createRef();
 
-  constructor() {
-    super();
-    this.state = {
-      userName: "waterfairy",
-      password: "123456",
-      isDialogChange: false,
-      loadingDialogText: "",
-      loadingDialogVisible: false,
-    };
-  }
+    dialogRef = React.createRef();
 
-  //登陆
-  onLoginPress() {
-    if (this.state.userName == null) {
-      alert("请输入帐号");
-    } else if (this.state.password == null) {
-      alert("请输入密码");
-    } else {
-      //展示dialog
-      this.dialogRef.current.show();
-      this.requestLogin().then((r) => {
-        this.dialogRef.current.dismiss();
-        alert("登录成功:" + r);
-      });
+    constructor() {
+        super();
+        this.state = {
+            userName: "",
+            password: "",
+            isDialogChange: false,
+            loadingDialogText: "",
+            loadingDialogVisible: false,
+        };
+        this.getAccountInfo();
     }
-  }
 
-  /**
-   * 登录
-   * */
-  async requestLogin() {
-    const url = BASE_URL + "/user/login";
-    const params = {
-      account: this.state.userName,
-      password: this.state.password,
-    };
+    /**
+     * 获取帐号信息
+     */
+    getAccountInfo() {
+        AsyncStorage.getItem("userName")
+            .then((userName) => {
+                this.setState({ userName: userName });
+            });
+        AsyncStorage.getItem("password")
+            .then((password) => {
+                this.setState({ password: password });
+            });
+    }
 
-    const options = {
-      method: "POST",
-      body: params,
-    };
-    let result = await fetch(url, options);
-    result = await result.json();
-    return result;
-  }
+    /**
+     * 保存帐号信息
+     */
+    saveAccountInfo() {
+        AsyncStorage.setItem("userName", this.state.userName);
+        AsyncStorage.setItem("password", this.state.password);
+        console.log(this.state.userName + "_" + this.state.password);
+    }
 
-  render() {
-    return <View
-      style={styles.root}>
-      {/*加载dialog */
-      }
-      <LoadingDialog text={this.state.loadingDialogText}
-                     visible={this.state.loadingDialogVisible}
-                     cancelable={true}
-                     ref={this.dialogRef} />
-      {/* 状态栏 */
-      }
-      <StatusBar backgroundColor="#981090" />
-      {/* logo */
-      }
-      <Image
-        style={styles.logo}
-        source={require("../../resources/images/dog.png")} />
-      {/* 帐号 */
-      }
-      <View
-        style={styles.input_form}>
-        <Image
-          style={styles.image}
-          source={require("../../resources/images/user.png")} />
-        <TextInput
-          style={styles.input}
-          placeholder="请输入帐号"
-          defaultValue={this.state.userName}
-          onChangeText={(text) => this.state.userName = text} />
-      </View>
-      {/* 密码 */
-      }
-      <View style={styles.input_form}>
-        <Image
-          style={styles.image}
-          source={require("../../resources/images/pwd.png")} />
-        <TextInput
-          style={styles.input}
-          textContentType="password"
-          secureTextEntry={true}
-          defaultValue={this.state.password}
-          placeholder="请输入密码"
-          onChangeText={(text) => this.state.userName = text} />
-      </View>
-      {/* 登录 */
-      }
-      <Button
-        style={styles.login}
-        title="登录"
-        onPress={() => this.onLoginPress()} />
-    </View>;
-  }
+    /**
+     *登陆
+     */
+    onLoginPress() {
+        if (this.state.userName == null) {
+            alert("请输入帐号");
+        } else if (this.state.password == null) {
+            alert("请输入密码");
+        } else {
+            //展示dialog
+            this.dialogRef.current.show();
+            this.requestLogin();
+        }
+    }
+
+    /**
+     * 登录
+     * */
+    requestLogin() {
+        const url = BASE_URL + "/user/login";
+
+        let formBody = new FormData();
+        formBody.append("account", this.state.userName);
+        formBody.append("password", this.state.password);
+
+        let jsonBody = JSON.stringify({
+            account: this.state.userName,
+            password: this.state.password,
+        });
+
+        console.log(JSON.stringify(formBody));
+
+        fetch(url, {
+            method: "POST", //请求方法 默认get
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "multipart/form-data", //数据格式 json或者key-value形式
+            },
+            body: formBody,
+        })
+            .then(response => {
+                console.log("status->" + response.status);
+                if (response.ok) {
+                    console.log("请求ok");
+                } else {
+                    console.log("请求不ok");
+                }
+                return response.json();
+            })
+            .then((result) => {
+                this.saveAccountInfo();
+                console.log(result);
+            })
+            .catch(err => {
+                console.log("请求失败->" + err.toString());
+            })
+            .done(result => {
+                console.log("请求完成");
+                this.dialogRef.current.dismiss();
+            });
+    }
+
+    render() {
+        return <View
+            style={styles.root}>
+            {/*加载dialog */}
+            <LoadingDialog text={this.state.loadingDialogText}
+                           visible={this.state.loadingDialogVisible}
+                           cancelable={true}
+                           ref={this.dialogRef} />
+            {/* 状态栏 */}
+            <StatusBar backgroundColor="#981090" />
+            {/* logo */}
+            <Image
+                style={styles.logo}
+                source={require("../../resources/images/dog.png")} />
+            {/* 帐号 */}
+            <View
+                style={styles.input_form}>
+                <Image
+                    style={styles.image}
+                    source={require("../../resources/images/user.png")} />
+                <TextInput
+                    style={styles.input}
+                    placeholder="请输入帐号"
+                    defaultValue={this.state.userName}
+                    onChangeText={(text) => this.state.userName = text} />
+            </View>
+            {/* 密码 */}
+            <View style={styles.input_form}>
+                <Image
+                    style={styles.image}
+                    source={require("../../resources/images/pwd.png")} />
+                <TextInput
+                    style={styles.input}
+                    textContentType="password"
+                    secureTextEntry={true}
+                    defaultValue={this.state.password}
+                    placeholder="请输入密码"
+                    onChangeText={(text) => this.state.password = text} />
+            </View>
+            {/* 登录 */}
+            <Button
+                style={styles.login}
+                title="登录"
+                onPress={() => this.onLoginPress()} />
+        </View>;
+    }
+
 }
 
 const styles = StyleSheet.create({
-  logo: {
-    borderRadius: 150,
-    width: 150,
-    height: 150,
-  },
-  root: {
-    alignItems: "center",
-    backgroundColor: "#981090",
-    paddingTop: "30%",
-    height: "100%",
-  },
-  input_form: {
-    marginTop: 20,
-    width: "90%",
-    flexDirection: "row",
-    height: 50,
-    alignItems: "center",
-    backgroundColor: "white",
-    borderRadius: 10,
-  },
-  image: {
-    left: 10,
-    width: 20,
-    height: 20,
-  },
-  input: {
-    left: 20,
-    height: 40,
-    width: "85%",
-  },
-  login: {
-    marginTop: 40,
-    width: "90%",
-    height: 50,
-  },
+    logo: {
+        borderRadius: 150,
+        width: 150,
+        height: 150,
+    },
+    root: {
+        alignItems: "center",
+        backgroundColor: "#981090",
+        paddingTop: "30%",
+        height: "100%",
+    },
+    input_form: {
+        marginTop: 20,
+        width: "90%",
+        flexDirection: "row",
+        height: 50,
+        alignItems: "center",
+        backgroundColor: "white",
+        borderRadius: 10,
+    },
+    image: {
+        left: 10,
+        width: 20,
+        height: 20,
+    },
+    input: {
+        left: 20,
+        height: 40,
+        width: "85%",
+    },
+    login: {
+        marginTop: 40,
+        width: "90%",
+        height: 50,
+    },
 });
